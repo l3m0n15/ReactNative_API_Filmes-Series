b5a65e59cfea9847834af04946195902b28a5a7b
André Ryan <sn.andreryan@live.com>
2025-09-23 00:10:27 -0300

    auto commit

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Button, View} from 'react-native';
+import{ButtonOne} from '../components/ButtonMain';
+
+export const LoginScreen =() => {
+return(
+
+<View>
+
+<ButtonMain title="Register" backgroundColor="#1DB954" textColor="#FFFFFF" />
+<ButtonMain title="Login" backgroundColor="#191414" textColor="#FFFFFF" />
+
+
+
+
+</View>
+
+)
+
+}
