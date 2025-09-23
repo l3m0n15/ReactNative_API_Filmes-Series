3af2cc0f54d51851dc70b7616502acbcb02a7d7a
André Ryan <sn.andreryan@live.com>
2025-09-23 00:15:01 -0300

    auto commit

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+// colors.ts
+export const Colors = {
+  spotifyGreen: '#1DB954',
+  spotifyBlack: '#191414',
+  white: '#FFFFFF',
+  black: '#000000',
+  gray: '#888888',
+  // outras cores do projeto
+};
