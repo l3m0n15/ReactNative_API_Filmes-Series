1deb540bf37987b1766adb94baad5e92b8a7ee55
André Ryan <sn.andreryan@live.com>
2025-09-23 00:05:53 -0300

    auto commit

--- src/components/ButtonMain.tsx
+++ src/components/ButtonMain.tsx
@@ -3,14 +3,14 @@ import {TouchableOpacity,Text} from 'react-native';
 import { Styles } from "../styles/ButtonMain"; //importar estilos de outro arquivo
 
 //Definindo os tipos das props do botão
-interface ButtonMainProps {
+interface ButtonOneProps {
 title:string  //Texto que vai aparecer dentro do botão
 onPress: () => void // Função que será executada quando o botão for pressionado
 
 }
 
 //Componente funcional do botão
-export const ButtonMain: React.FC<ButtonMainProps> = ({title, onPress}) => {
+export const ButtonOne: React.FC<ButtonOneProps> = ({title, onPress}) => {
     return(
 //TouchableOpacity é o botão "clicável" que dá efeito de opacidade ao ser pressionado
 <TouchableOpacity style={Styles.button}  //Aplica os estilos definidos da pasta "Styles./ButtonMain.ts"
