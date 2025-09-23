bf5d93f4474f5045bf39e75f9f48d78608df0acc
André Ryan <sn.andreryan@live.com>
2025-09-22 23:40:33 -0300

    auto commit

--- src/components/ButtonMain.tsx
+++ src/components/ButtonMain.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {TouchableOpacity,Text} from 'react-native';
-import { Style, Styles } from "../styles/ButtonMain"; //importar estilos de outro arquivo
+import { Styles } from "../styles/ButtonMain"; //importar estilos de outro arquivo
 
 //Definindo os tipos das props do botão
 interface ButtonVerdeProps {
