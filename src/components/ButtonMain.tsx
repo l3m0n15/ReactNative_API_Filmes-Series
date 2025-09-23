b284d7d1225e6cbaa2076a9e97b19ef5220b870a
André Ryan <sn.andreryan@live.com>
2025-09-22 23:26:18 -0300

    auto commit

--- src/components/ButtonMain.tsx
+++ src/components/ButtonMain.tsx
@@ -21,7 +21,6 @@ activeOpacity={0.7}  //Define a transparência quando o botão é pressionado
 {/* Texto dentro do botão*/ }
 <text style={Style.text}> Login </text>
 
-
 </Touchableopacity>
     )
 }
---
+++ src/styles/ButtonMain.ts
@@ -0,0 +1,21 @@
+//Estilo do botão Main
+
+import {StyleSheet} from "react-native";
+
+const Style = StyleSheet.create({
+button:{
+backgroundColor: '#1DB954', //cor de fundo (verde)
+paddingVertical: 14, // Espaço interno em cima e embaixo
+paddingHorizontal:40, // Espaço interno nas laterais 
+borderRadius: 50, //Deixa o botão com bordas arrendadas
+alignItems: 'center', // Centraliza o conteúdo horizontalmente
+justifyContent:'center', // Centraliza o conteúdo verticalmente
+
+
+
+
+
+}
+
+
+})
