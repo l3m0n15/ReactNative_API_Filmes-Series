6d01e3c4d33de51f03a101758c4b004010eba2c6
André Ryan <sn.andreryan@live.com>
2025-09-23 00:11:29 -0300

    auto commit

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import{ButtonOne} from '../components/ButtonMain';
 
 export const LoginScreen =() => {
